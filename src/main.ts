052eb9550eb5c35cb07db517458938fa09b4d501
Philip Grabenhorst <phlip007@gmail.com>
2020-08-08 17:11:33 -0700

    Add LogRocket Monitoring in Production

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+const LogRocket = require('logrocket');
+
 if (environment.production) {
   enableProdMode();
+  LogRocket.init('wgjuww/ear-training');
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
